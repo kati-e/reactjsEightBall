52269c4b766396ba119cc50779bc1549db1992aa
Katie <clearykatelyn@gmail.com>
2023-04-25 13:28:33 +1000

    added basic stuff to manage the answer - no random answers implemented yet just a test

--- eightball/README.md
+++ eightball/README.md
@@ -1,6 +1,11 @@
-Simple Eight Ball React Web Application. 
+Simple Eight Ball React.js Web Application. 
+I made a very very simple javascript version of an eightball console app, and decided to make it into a react app so it can be more interactive :)
+
+Rules:
+1. Enter your question through the input box and click 'Go!'
+2. Enjoy your random eightball answer (its wisdom is final)
 
 Just for fun and practise!
 
 kati-e
-Last updated: 23/4/23
+Last updated: 25/4/23
--- eightball/src/App.js
+++ eightball/src/App.js
@@ -5,10 +5,16 @@ function App() {
   const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
 
-  function handleSubmit(event){
+  function handleQuestionSubmit(event){
     event.preventDefault();
     const inputVal = event.target.question.value;
     setQuestion(inputVal);
+    setAnswer('');
+  }
+
+  function generateAnswer() {
+    const answer = 'test';
+    setAnswer(answer);
   }
 
   return (
@@ -16,15 +22,16 @@ function App() {
       <h1>Eight Ball</h1>
       <p>Welcome to my simple Eight Ball application. <br/>Ask it a yes/no question and it will provide the answer you seek.</p>
       <h3>Your Question:</h3>
-      <form id="myForm" onSubmit={handleSubmit}>
+      <form id="myForm" onSubmit={handleQuestionSubmit}>
          <div className="myForm_inputs">
           <input className="input" name="question" type="text"></input>
-          <button for="myForm" className="button" name="submit" type="submit">Go!</button>
+          <button className="button" name="submit" type="submit">Go!</button>
          </div>
       </form>
       {question ? <p>Question: '{question}'</p> : null}
+      {question && !answer ? <button className="button" onClick={generateAnswer}>Generate Answer Now</button> : null}
       {answer ? <p className='answers'>Answer: '{answer}'</p> : null}
-      {question && answer ? <p><br/>My wisdom is final. Do what you will with this information.</p> : <p>Go ahead, ask your question.</p>}
+      {answer ? <p><br/>My wisdom is final. Do what you will with this information.</p> : <p>Go ahead, ask your question.</p>}
     </div>
   );
 }
