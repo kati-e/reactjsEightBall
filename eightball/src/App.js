8a47877635e4defbc7625b346605c9d1b0289b8f
Katie <clearykatelyn@gmail.com>
2023-04-25 12:20:35 +1000

    got the little question input box working finally

--- eightball/src/App.js
+++ eightball/src/App.js
@@ -1,41 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 
-//Input of Question & Creating Form Module
-function InputQuestion(props) {
-  const [question, setQuestion] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); //avoiding the default action
-    props.onQuestionSubmit(question); //grabbing the current question value
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" id="eightball-question" value={question} onChange={(e) => setQuestion(e.target.value)}/>
-      <button id="go-button" type="submit">Go!</button>
-    </form>
-  );
-}
-
 function App() {
   const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
 
-  const handleQuestionSubmit = (question) => {
-    console.log(`setQuestion = ${question}`);
-    setQuestion(question);
-  };
+  function handleSubmit(event){
+    event.preventDefault();
+    const inputVal = event.target.question.value;
+    setQuestion(inputVal);
+  }
 
   return (
     <div className="App">
       <h1>Eight Ball</h1>
-      <p>
-        Welcome to my simple Eight Ball application. <br />
-        Ask it a question and it will provide the answer you seek.
-      </p>
+      <p>Welcome to my simple Eight Ball application. <br/>Ask it a question and it will provide the answer you seek.</p>
       <h3>Your Question:</h3>
-      <InputQuestion onQuestionSubmit={handleQuestionSubmit} />
-      <p>{question}</p>
+      <form onSubmit={handleSubmit}>
+         <input name="question" type="text"></input>
+         <button name="submit" type="submit">Go!</button>
+      </form>
+      {question ? <p>Question: '{question}'</p> : null}
+      {answer ? <p className='answers'>Answer: '{answer}'</p> : null}
+      <p><br/>My wisdom is final. Do what you will with this information.</p>
     </div>
   );
 }
